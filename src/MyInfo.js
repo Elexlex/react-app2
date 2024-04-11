45257f3f6cb38197c1253d5bc803773368cf5b79
Elexlex <voya2705@gmail.com>
2024-04-11 21:21:50 +0300

    Done

---
+++ src/MyInfo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function MyInfo() {
+  return (
+    <div className="container">
+      <h1>Я пашочек</h1>
+      <p>Коротка розповідь про себе</p>
+      <h2>Місця, котрі я хотів би побачити:</h2>
+      <ul>
+        <li>Канада</li>
+        <li>Фінляндія</li>
+        <li>Латвія</li>
+      </ul>
+    </div>
+  );
+}
+
+export default MyInfo;
